e0c00d6175ab2948b7f8adba062fd6bf76279e6c
José Leonan S. Carvalho <j.leonancarvalho@gmail.com>
2019-10-01 20:46:39 -0300

    Implements login

--- src/L2jBrasil/L2JPHP/Models/Dist/Interlude/L2jMobius/Players/Accounts.php
+++ src/L2jBrasil/L2JPHP/Models/Dist/Interlude/L2jMobius/Players/Accounts.php
@@ -9,6 +9,7 @@ namespace L2jBrasil\L2JPHP\Models\Dist\Interlude\L2jMobius\Players;
 
 
 use L2jBrasil\L2JPHP\Models\AbstractBaseModel;
+use L2jBrasil\L2JPHP\Models\AbstractSQL;
 
 class Accounts extends AbstractBaseModel implements \L2jBrasil\L2JPHP\Models\Interfaces\Players\Accounts
 {
@@ -27,10 +28,39 @@ class Accounts extends AbstractBaseModel implements \L2jBrasil\L2JPHP\Models\Int
     ];
 
 
+    /**
+     * @param $id
+     * @return bool|AbstractSQL|mixed
+     */
     public function ban($id)
     {
         return $this->update($id, [
             "access_level" => -1
         ]);
     }
+
+    /**
+     *
+     * @param $login
+     * @param $password
+     * @return mixed
+     *
+     *
+     */
+    public function login($login, $password)
+    {
+        $pass1 = base64_encode(pack('H*', sha1(trim($password))));
+        $pass2 = base64_encode(hash('whirlpool', trim($password), true));
+
+
+        $loginCol = $this->translate("login");
+        $passwordCol = $this->translate("password");
+        $account = $this->select()
+            ->where("{$loginCol} == {$login}  AND ({$passwordCol} = '{$pass1}' OR {$passwordCol} = '{$pass2}')")
+            ->query()
+            ->Fetch();
+
+        return $account;
+    }
+
 }
