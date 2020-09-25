5e216a67fcb9a4d7b10fe285f439c54aaf03c11d
Leonan Carvalho <j.leonancarvalho@gmail.com>
2020-09-25 15:41:54 -0300

    Clean up clan data model methods

--- src/L2jBrasil/L2JPHP/Models/Dist/Interlude/L2JSERVER/Clan/ClanData.php
+++ src/L2jBrasil/L2JPHP/Models/Dist/Interlude/L2JSERVER/Clan/ClanData.php
@@ -6,26 +6,6 @@ use L2jBrasil\L2JPHP\Models\AbstractBaseModel;
 class ClanData  extends AbstractBaseModel implements \L2jBrasil\L2JPHP\Models\Interfaces\Clan\ClanData
 {
 
-    public function all()
-    {
-        // TODO: Implement all() method.
-    }
-
-    public function get($id)
-    {
-        // TODO: Implement get() method.
-    }
-
-    public function delete($id)
-    {
-        // TODO: Implement delete() method.
-    }
-
-    public function update($id, $data)
-    {
-        // TODO: Implement update() method.
-    }
-
     public function getReputation($id)
     {
         // TODO: Implement getReputation() method.
